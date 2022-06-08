525e38a05370f439a88be3ea09a2d661006b47cd
joycodes <jnguttu@gmail.com>
2022-06-08 14:01:33 +0300

    mode production

--- instagram/settings.py
+++ instagram/settings.py
@@ -161,17 +161,17 @@ LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'login'
 
 
-# EMAIL_USE_TLS=config('EMAIL_USE_TLS', cast=bool)  
-# EMAIL_HOST=config('EMAIL_HOST')
-# EMAIL_PORT=config('EMAIL_PORT',cast=int)
-# EMAIL_HOST_USER=config('EMAIL_HOST_USER')  
-# EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')  
+EMAIL_USE_TLS=config('EMAIL_USE_TLS', cast=bool)  
+EMAIL_HOST=config('EMAIL_HOST')
+EMAIL_PORT=config('EMAIL_PORT',cast=int)
+EMAIL_HOST_USER=config('EMAIL_HOST_USER')  
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')  
 
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
-EMAIL_HOST = config('EMAIL_HOST', default='localhost')
-EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+# EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+# EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
